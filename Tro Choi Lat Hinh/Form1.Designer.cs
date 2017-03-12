3119f0d1cde8ce9b466b1408a73b0684c2e85b38
hthhello <hthhello@gmail.com>
2017-03-13 01:17:11 +0700

    First

---
+++ Tro Choi Lat Hinh/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Tro_Choi_Lat_Hinh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Delay = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Delay
+            // 
+            this.Delay.Tick += new System.EventHandler(this.Delay_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(88, 711);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(43, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "20";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label2.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(631, 708);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "R.S";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Impact", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Khaki;
+            this.label5.Location = new System.Drawing.Point(305, 708);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 36);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Impact", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(204, 709);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 36);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Score :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(21, 716);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Time :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(127, 719);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 23);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "+10";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("UTM Swiss 721 Black Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(376, 701);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(216, 46);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Lật Hình Game";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(699, 761);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(715, 800);
+            this.MinimumSize = new System.Drawing.Size(715, 800);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(7);
+            this.Text = "HTH";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Delay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Tro Choi Lat Hinh/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tro_Choi_Lat_Hinh
+{
+    public partial class Form1 : Form
+    {
+        TableLayoutPanel table = new TableLayoutPanel();
+        int so = 4;
+        int[] ID;
+        bool[] State;
+        int ID1 = -1;
+        int ID2 = -1;
+        int time = 20;
+        int IDk1;
+        int IDk2;
+        double score = 0;
+        int ctime = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void Init()
+        {
+            ID = new int[so * so];
+            State = new bool[so * so];
+            for (int i = 0; i < so*2; i++) // dem so hinh
+            {
+                ID[i] = 0;
+                State[i] = false;
+            }
+            int dem = 0;
+            for (int i = 0; i < so*2; i++)
+            {
+                do
+                {
+                    Random random = new Random();
+                    int IDx = random.Next() % (so*so);
+                    if (ID[IDx] == 0)
+                    {
+                        dem++;
+                        ID[IDx] = i;
+                    }
+                } while (dem < 2);
+                dem = 0;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table.ColumnCount = so;
+            table.RowCount = so;
+            table.Dock = DockStyle.Fill;
+            for (int i = 0; i < so*so; i++)
+            {
+                PictureBox pict = new PictureBox();
+                pict.Name = i.ToString();
+                pict.Image = Image.FromFile("HTH.jpg");
+                pict.Margin = new Padding(1, 1, 1, 1);
+                pict.Width = 170;
+                pict.Height = 170;
+                pict.Click += PictureBox_Click;
+                table.Controls.Add(pict);
+            }
+            this.Controls.Add(table);
+            Init();
+            
+            
+        }
+        private void reDraw()
+        {
+            DoubleBuffered = true;
+            table.Controls.Clear();
+            table.ColumnCount = so;
+            table.RowCount = so;
+            table.Dock = DockStyle.Fill;
+            for (int i = 0; i < so*so; i++)
+            {
+                PictureBox pict = new PictureBox();
+                pict.Name = i.ToString();
+                if (State[i] == false)
+                {
+                    pict.Image = Image.FromFile("HTH.jpg");
+                    pict.Click += PictureBox_Click;
+                }
+                else
+                {
+                    BackColor = Color.DarkCyan;
+                    pict.Image = Image.FromFile(ID[i] + ".jpg");
+                }
+                pict.Margin = new Padding(1, 1, 1, 1);
+                pict.Width = 170;
+                pict.Height = 170;
+                table.Controls.Add(pict);
+            }
+            this.Controls.Add(table);
+            
+        }
+        
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            PictureBox photo = (PictureBox)sender;
+            
+            if (ID1 == -1)
+            {
+                timer1.Enabled = true;
+                timer1.Start();
+                ID1 = Int32.Parse(photo.Name);
+                photo.Image = Image.FromFile(ID[ID1] + ".jpg");
+                photo.Click -= PictureBox_Click;
+            }
+            else
+            {
+               
+                ID2 = Int32.Parse(photo.Name);
+                photo.Image = Image.FromFile(ID[ID2] + ".jpg");
+                Delay.Enabled = true;
+                Delay.Start();
+                
+            }
+        }
+
+     
+        private void Delay_Tick(object sender, EventArgs e)
+        {
+            Delay.Stop();
+            if (ID[ID1] == ID[ID2])
+            {
+                    State[ID1] = true;
+                    State[ID2] = true;
+                    IDk1 = ID1;
+                    IDk2 = ID2;
+                   ID1 = -1; ID2 = -1;
+                reDraw();
+                if (time > 0)
+                {
+                    score = score + 1.25;
+                    label5.Text = score.ToString();
+                }
+            }
+            else
+            {
+                State[ID1] = false;
+                State[ID2] = false;
+                ID1 = -1; ID2 = -1;
+                reDraw(); 
+            }
+            int Game = 1;
+            for (int i = 0; i < 16; i++)
+            {
+                if (State[i] == false)
+                {
+                    Game = 0;
+                    break;
+                }
+            }
+            if (Game == 1 && time > 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Win ^^!\n Hit R.S to Play Again", "HTH");
+                
+            }
+        
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (time > 0)
+            {
+                time--;
+                label1.Text = time.ToString();
+            }
+            if (time <= 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Time Out! \nHit R.S or see this message again =]]");
+            }
+
+           
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            time = 20;
+            score = 0;
+            timer1.Stop();
+            label1.Text = "30";
+            for (int i = 0; i < 16; i++)
+            {
+                State[i] = false;
+                ID1 = -1; ID2 = -1;
+            }
+            timer1.Stop();
+            Init();
+            reDraw(); 
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            ctime++;
+            if(ctime == 1)
+            {
+                time = time + 10;
+            }
+            if(ctime > 1)
+            {
+                MessageBox.Show("I can only help you one time", "HTH");
+            }
+        }  
+        }
+    }
+    
+  
